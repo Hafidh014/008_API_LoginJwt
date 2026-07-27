27bed87a0312a9afe7ef1b653d8e5dbdf4b434ee
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-07-27 15:49:29 +0700

    Cek user existing dan hash password

--- controller/userController.js
+++ controller/userController.js
@@ -13,3 +13,19 @@ async function register(req, res) {
             });
         }
 
+        const existingUser = await user.findOne({
+            where: { email }
+        });
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: 'Email sudah terdaftar'
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await db.User.create({
+            email,
+            password: hashedPassword
+        });
