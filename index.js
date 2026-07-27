714c0756bf53a127b94341d4bc013f95049c8898
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-07-27 14:53:13 +0700

    menambahkan konfigurasi awal server Express

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDatabase = require('./config/Db');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/komik', require('./routes/api'));
+
+async function startServer() {
+    await connectDatabase();
+    
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+startServer();
