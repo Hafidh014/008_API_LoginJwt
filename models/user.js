2d3170e6e370fa1b18ce9d9bd9562d62dbf2a78f
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-07-27 15:09:57 +0700

    membuat model User untuk autentikasi

---
+++ models/user.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, dataTypes) => {
+    const User = sequelize.define("user", {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email: {
+            type: dataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+});
+return User;
+};
